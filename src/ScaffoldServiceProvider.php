76eebb7a61cd6ed469a34509dab5d773b05b3c70
Audun Larsen <larsen@xqus.com>
2020-03-30 20:25:48 +0200

    Adds scaffold...

---
+++ src/ScaffoldServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Boonei\Scaffold;
+
+use Illuminate\Support\ServiceProvider;
+
+class ScaffoldServiceProvider extends ServiceProvider
+{
+
+  /**
+   * Register any application services.
+   *
+   * @return void
+   */
+  public function register()
+  {
+
+  }
+
+  /**
+   * Bootstrap any package services.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+
+  }
+}
