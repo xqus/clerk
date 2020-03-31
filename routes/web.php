7900342f2ec86f94e0ccea0b5353bc98933cf361
Audun Larsen <larsen@xqus.com>
2020-03-31 10:38:52 +0200

    Adds routes for saving profile and password

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Boonei\Scaffold\Http\Controllers\ProfileController;
 
 Route::get('/profile', [ProfileController::class, 'index'])
      ->name('scaffold.profile');
+
+Route::patch('/profile', [ProfileController::class, 'save'])
+      ->name('scaffold.profile.patch');
+
+Route::patch('/profile/password', [ProfileController::class, 'savePassword'])
+      ->name('scaffold.profile.password.patch');
