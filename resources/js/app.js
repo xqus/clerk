0b6a9b391abf785f7ad0f25a4a4d05c71b05d91c
Audun Larsen <larsen@xqus.com>
2020-08-23 15:02:33 +0200

    Refeactor a bit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,3 @@
-Vue.component('subscription-management', require('./components/SubscriptionManagement.vue').default);
 Vue.component('payment-method', require('./components/PaymentMethod.vue').default);
 
 Vue.mixin(require('./trans'));
