8ac301b394694cdb65c552f68893b6ec89bf4ed9
Audun Larsen <larsen@xqus.com>
2020-08-30 20:15:04 +0200

    Removes unused routes.

--- routes/web.php
+++ routes/web.php
@@ -6,23 +6,6 @@ use Boonei\Scaffold\Http\Controllers\ProfileController;
 Route::get('/profile', [ProfileController::class, 'index'])
      ->name('scaffold.profile');
 
-Route::patch('/profile', [ProfileController::class, 'save'])
-      ->name('scaffold.profile.patch');
-
-Route::patch('/profile/password', [ProfileController::class, 'savePassword'])
-      ->name('scaffold.profile.password.patch');
-
-/*Route::post('/profile/paymentmethod', [ProfileController::class, 'addPaymentMethod'])
-      ->name('scaffold.profile.paymentmethod.post');
-
-Route::delete('/profile/paymentmethod', [ProfileController::class, 'remPaymentMethod'])
-      ->name('scaffold.profile.paymentmethod.delete');
-
-Route::patch('/profile/subscription', [ProfileController::class, 'updateSubscription'])
-      ->name('scaffold.profile.subscription.patch');
-*/
-
-
 Route::group(['prefix' => 'api/v1'], function(){
     Route::get('/user/setup-intent', [ProfileController::class, 'getSetupIntent']);
     Route::get('/user/subscription-plans', [ProfileController::class, 'getSubscriptionPlans']);
