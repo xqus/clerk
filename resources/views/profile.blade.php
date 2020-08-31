e65ec2d5a73979accfebd80fec66645534dcfafa
Audun Larsen <larsen@xqus.com>
2020-08-31 12:48:50 +0200

    Cleans up head section.

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,23 +3,16 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
     <script src="{{ asset('boonei/scaffold/js/app.js') }}" defer></script>
-
-
-    <!-- Styles -->
-    <link href="{{ asset('boonei/scaffold/css/app.css') }}" rel="stylesheet">
     <script>
         window._locale = '{{ app()->getLocale() }}';
         window._translations = {!! cache('scaffold.translations') !!};
     </script>
+    <link href="{{ asset('boonei/scaffold/css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
 <div id="app" class="">
 
