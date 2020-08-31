92ebd6909d74a025527ad65c757cc868c9450a88
Audun Larsen <larsen@xqus.com>
2020-08-31 19:26:07 +0200

    Adds test scaffold

---
+++ tests/TestCase.php
@@ -0,0 +1,18 @@
+<?php
+namespace xqus\Clerk\Tests;
+
+use xqus\Clerk\ClerkServiceProvider;
+use Illuminate\Support\Facades\Artisan;
+
+class TestCase extends \Orchestra\Testbench\TestCase {
+
+    protected function getPackageProviders($app) {
+        return [
+            ClerkServiceProvider::class,
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app) {
+        // perform environment setup
+    }
+}
---
+++ tests/Unit/Test.php
@@ -0,0 +1,16 @@
+<?php
+namespace xqus\Clerk\Tests\Unit;
+
+use Illuminate\Support\Facades\Artisan;
+use xqus\Clerk\Tests\TestCase;
+use Illuminate\Support\Facades\Storage;
+
+
+class Test extends TestCase
+{
+    /** @test */
+    function lookup_works_1()
+    {
+        $this->assertTreu(true);
+    }
+}
