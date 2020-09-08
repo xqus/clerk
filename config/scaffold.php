829019919bfae4d0f86df7ca10fd8a125cfd9776
Audun Larsen <larsen@xqus.com>
2020-09-08 11:37:31 +0200

    Updates default config

--- config/scaffold.php
+++ config/scaffold.php
@@ -4,20 +4,16 @@ return [
     'stripe' => [
     ],
     'products' => [
-        'elearning' => [
-            'title' => 'E-læringspakke',
+        'default' => [
+            'title' => 'My subscription',
             'plans' => [
                 'plan_H11NJP6iH8xuwi' => [
-                    'price'    => '200,-',
-                    'interval' => 'årlig',
-                ],
-                'plan_H11Mz7NHfjdFR1' => [
-                    'price'    => '120,-',
-                    'interval' => 'halvårlig',
+                    'price'    => '$200',
+                    'interval' => 'yearly',
                 ],
                 'plan_H11La9QfZTCbSh' => [
-                    'price'    => '29,-',
-                    'interval' => 'månedlig',
+                    'price'    => '$29',
+                    'interval' => 'monthly',
                 ],
             ],
         ],
